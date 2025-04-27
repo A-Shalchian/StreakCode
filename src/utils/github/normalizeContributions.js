30d3d3d88d27a45aec7bded30a37db42741849a4
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-04-27 02:03:35 -0400

    removed comment

--- src/utils/github/normalizeContributions.js
+++ src/utils/github/normalizeContributions.js
@@ -1,26 +1,25 @@
-// ✅ utils/github/normalizeContributions.js
 function normalizeContributions(weeks) {
-    const dateMap = new Map();
-  
-    weeks.forEach((week) => {
-      week.contributionDays.forEach((day) => {
-        const contributionDate = new Date(day.date + 'T00:00:00Z');
-        const localDate = new Date(
-          contributionDate.getTime() + contributionDate.getTimezoneOffset() * 60000
-        );
-        const localDateISO = localDate.toISOString().split('T')[0];
-  
-        dateMap.set(localDateISO, {
-          count: day.contributionCount,
-          color: day.color,
-        });
-  
-        day.date = localDateISO;
+  const dateMap = new Map();
+
+  weeks.forEach((week) => {
+    week.contributionDays.forEach((day) => {
+      const contributionDate = new Date(day.date + "T00:00:00Z");
+      const localDate = new Date(
+        contributionDate.getTime() +
+          contributionDate.getTimezoneOffset() * 60000
+      );
+      const localDateISO = localDate.toISOString().split("T")[0];
+
+      dateMap.set(localDateISO, {
+        count: day.contributionCount,
+        color: day.color,
       });
+
+      day.date = localDateISO;
     });
-  
-    return { weeks, dateMap };
-  }
-  
-  module.exports = { normalizeContributions };
-  
+  });
+
+  return { weeks, dateMap };
+}
+
+module.exports = { normalizeContributions };
